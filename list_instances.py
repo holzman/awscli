6e77f4200da5a57361df47124199f60f40cd2ad1
Burt Holzman <burt@fnal.gov>
2017-10-26 11:18:10 -0500

    Add AMI aliases

--- list_instances.py
+++ list_instances.py
@@ -10,6 +10,8 @@ parser.add_option("-a", "--all", dest="all_users", action="store_true", default=
                   help="list instances for all users")
 parser.add_option("-r", "--running", dest="running", action="store_true", default=False,
                   help="list only running instances")
+parser.add_option("-b", "--bare-ami", dest="bare_ami", action="store_true", default=False,
+                  help="list bare AMI instead of aliases")
 
 (options, args) = parser.parse_args()
 
@@ -39,6 +41,12 @@ instances = ec2.instances.filter(Filters=filters)
 out1 = ''
 out2 = ''
 
+amimap = {'ami-0ee61876': 'FPGA HDK',
+          'ami-72ed1e0a': 'DL 3.1  ',
+          'ami-296e7850': 'DL 2.2  '}
+
+if options.bare_ami:
+    amimap = {}
 for instance in instances:
 
     tag_str = ''
@@ -51,12 +59,16 @@ for instance in instances:
     lifetime = datetime.timedelta(minutes=int(lifetime))
 
     kill = (instance.state.get("Name") == 'running') and (now-lt) > lifetime
+
     outstr = "%s\t%s\t%8s\t%s\t%s\t" % (instance.state.get("Name"), instance.public_ip_address, tag_str, instance.id, instance.instance_type)
+    outstr += "%s\t" % amimap.get(instance.image_id, instance.image_id)
     outstr += "%s  %s\t%s\n" % (lt, lifetime, kill)
     out1 += outstr
 
 
+
 print "%s\t%s\t\t%8s\t%s\t\t\t%s" % ('State', 'IP', 'Owner', 'ID', 'type'),
+print "\t\t%s" % ('AMI'),
 print "\t\t%s    %s\t\t%s\n" % ('Launch_time (UTC)', 'Lifetime', 'Kill?')
 
 print out1
